37287c37f11e6a5435c2847544af8896b4cd29f4
alfred-bidokus <88543709+alfred-bidokus@users.noreply.github.com>
2021-08-06 15:33:51 +0200

    Add files via upload

--- test.go
+++ test.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hallo")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
