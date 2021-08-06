9d3a6fa4928fdde95e41919613500ad26db1f3e4
alfred-bidokus <88543709+alfred-bidokus@users.noreply.github.com>
2021-08-06 16:20:43 +0200

    Add files via upload

--- test.go
+++ test.go
@@ -1,19 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
+	"net"
 )
 
+/*
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "hallo")
 }
+*/
+
+func handleConnection(conn net.Conn) {
+	buf := bufio.NewWriter(conn)
+	buf.WriteString("hallo")
+	buf.Flush()
+	conn.Close()
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	fmt.Println(port)
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":"+port, nil)
+	port := "80"
+	//fmt.Println(port)
+	//http.HandleFunc("/", hello)
+	//http.ListenAndServe(":"+port, nil)
+	ln, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Println("rip")
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("rip")
+		}
+		go handleConnection(conn)
+	}
 }
